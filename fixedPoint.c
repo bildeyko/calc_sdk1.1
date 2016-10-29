56ae4223f3040825983629d659388a49b7907ac7
Ilya Mikhaylov <ellepsis07@gmail.com>
2016-10-29 22:04:45 +0300

    Initial commit

---
+++ fixedPoint.c
@@ -0,0 +1,12 @@
+typedef unsigned char byte;
+
+byte bytes_cnt; //count of bytes for number
+byte point_pos; //point position in number
+
+
+
+char* add(char* first, char* second){
+	for (char i = bytes_cnt-1; i<point_pos; i++){
+		
+	}
+}
